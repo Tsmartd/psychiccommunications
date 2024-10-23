e722deba80ee6e3c5c67fd5f40a91e06c8663828
Tsmartd <lukmandurojaiye222@gmail.com>
2024-10-23 11:18:05 +0100

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,5 @@
+// Add event listener to schedule reading button
+document.querySelector('button').addEventListener('click', () => {
+    // Open contact page or scheduling tool
+    window.location.href = 'contact.html';
+  });
